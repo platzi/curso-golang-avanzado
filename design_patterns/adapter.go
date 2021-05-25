b308cb179335d9c7619ad777002006b8bb9df61e
Nestor Escoto <nestor@smartifice.com>
2021-05-25 17:32:39 -0600

    Adapter

---
+++ design_patterns/adapter.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type Payment interface {
+	Pay()
+}
+
+type CashPayment struct{}
+
+func (CashPayment) Pay() {
+	fmt.Println("Payment using Cash")
+}
+
+func ProcessPayment(p Payment) {
+	p.Pay()
+}
+
+type BankPayment struct{}
+
+func (BankPayment) Pay(bankAccount int) {
+	fmt.Printf("Paying using Bankaccount %d\n", bankAccount)
+}
+
+type BankPaymentAdapter struct {
+	BankPayment *BankPayment
+	bankAccount int
+}
+
+func (bpa *BankPaymentAdapter) Pay() {
+	bpa.BankPayment.Pay(bpa.bankAccount)
+}
+
+func main() {
+	cash := &CashPayment{}
+	ProcessPayment(cash)
+	// bank := &BankPayment{}
+	// ProcessPayment(bank)
+	bpa := &BankPaymentAdapter{
+		bankAccount: 5,
+		BankPayment: &BankPayment{},
+	}
+	ProcessPayment(bpa)
+}
