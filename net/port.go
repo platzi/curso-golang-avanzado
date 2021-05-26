315f0d29455b9af9f0e6da8e9d98e527d0a429fc
Nestor Escoto <nestor@smartifice.com>
2021-05-26 13:32:05 -0600

    Escaneador de Puertos

---
+++ net/port.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	for i := 0; i < 100; i++ {
+		// 1, 2, ..4, 99
+		// sitio:1, sitio:2, sitio:99, ...,
+		// 1 -> Open,  2 -> Closed, ...
+		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", i))
+		if err != nil {
+			continue
+		}
+		conn.Close()
+		fmt.Printf("Port %d is open\n", i)
+	}
+}
