4f7854e0757d0b8360a869e379e7927dff44bf51
Nestor Escoto <nestor@smartifice.com>
2021-05-26 13:56:12 -0600

    Escaneador de puertos concurrente

--- net/port.go
+++ net/port.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"sync"
 )
 
+var site = flag.String("site", "scanme.nmap.org", "url to scan")
+
 func main() {
+	flag.Parse()
+	var wg sync.WaitGroup
 	for i := 0; i < 65535; i++ {
-		// 1, 2, ..4, 99
-		// sitio:1, sitio:2, sitio:99, ...,
-		// 1 -> Open,  2 -> Closed, ...
-		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", i))
-		if err != nil {
-			continue
-		}
-		conn.Close()
-		fmt.Printf("Port %d is open\n", i)
+		wg.Add(1)
+		go func(port int) {
+			defer wg.Done()
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", *site, port))
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("Port %d is open\n", port)
+		}(i)
 	}
+	wg.Wait()
 }
